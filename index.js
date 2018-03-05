98b74edee886df433f00eb323586f44f3e8685ad
v <benpou2012@gmail.com>
2018-03-04 22:23:04 -0800

    set up keys in passport

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const passport = require('passport');
 const GoogleStratrgy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStratrgy());
+passport.use(new GoogleStratrgy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret
+}));
 
 app.get('/', (req, res) => {
   res.send({bye: 'buddy'})
